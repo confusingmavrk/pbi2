bb79811bb0e4e28a14d1a895abbf4be2a361de62
confusingmavrk <85229204+confusingmavrk@users.noreply.github.com>
2025-12-25 20:49:14 +0530

    Committing 1 item from workspace 21e72612-73c1-45d8-94b9-87be8163a090

--- Notebook 1.Notebook/notebook-content.py
+++ Notebook 1.Notebook/notebook-content.py
@@ -6,14 +6,27 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "cfe5c8eb-d442-4755-a46c-ca6c0d4025a8",
+# META       "default_lakehouse_name": "LH1",
+# META       "default_lakehouse_workspace_id": "6f2713a3-731f-4a96-bcc9-4508797f6966",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "cfe5c8eb-d442-4755-a46c-ca6c0d4025a8"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
-print("github_101")
+a= spark.sql(" insert into  LH1.dbo.t1 (col1,col2)  select 'e4' col1, 'f4' col2  ")
+b= spark.sql(" select * from LH1.dbo.t1  ")
+display(b)
 
 # METADATA ********************
 
